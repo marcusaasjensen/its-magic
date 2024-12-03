92291760b4bea53fc21fc3f3867dd2623bbdab58
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2024-12-03 15:56:42 +0100

    feat: add opacity animation

---
+++ Client/unity/Assets/Scripts/Transition/OpacityEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace Transition
+{
+    public class OpacityEffect : MonoBehaviour
+    {
+        public float opacitySpeed = 1.0f;
+        public float opacityMin = 0.2f;
+        public float opacityMax = 1.0f;
+
+        private SpriteRenderer _spriteRenderer;
+
+        private void Start()
+        {
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+
+       private void Update()
+        {
+            var alpha = Mathf.Lerp(opacityMin, opacityMax, (Mathf.Sin(Time.time * opacitySpeed) + 1) / 2);
+            var color = _spriteRenderer.color;
+            color.a = alpha;
+            _spriteRenderer.color = color;
+        }
+    }
+}
