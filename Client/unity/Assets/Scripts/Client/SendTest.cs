766864a202e45d2755472ff703aa72cbb3f79fd2
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2024-12-20 15:03:07 +0100

    feat: create switch scene transition

--- Client/unity/Assets/Scripts/Client/SendTest.cs
+++ Client/unity/Assets/Scripts/Client/SendTest.cs
@@ -20,5 +20,33 @@ namespace Client
             };
             WebSocketClient.Instance.SendMessageToServer(JsonUtility.ToJson(vibrateObject));
         }
+        
+        [ContextMenu("Light")]
+        public void getClock()
+        {
+            var getClock = new SwitchObjectMessage
+            {
+                clientId = "TopView",
+                type = "switchObject",
+                recipientId = "Android",
+                objectName = "alarmClock",
+                objectScene = "forest"
+            };
+            WebSocketClient.Instance.SendMessageToServer(JsonUtility.ToJson(getClock));
+        }
+        
+        [ContextMenu("Bag")]
+        public void getBag()
+        {
+            var getBag = new SwitchObjectMessage
+            {
+                clientId = "TopView",
+                type = "switchObject",
+                recipientId = "Android",
+                objectName = "bag",
+                objectScene = "forest"
+            };
+            WebSocketClient.Instance.SendMessageToServer(JsonUtility.ToJson(getBag));
+        }
     }
 }
