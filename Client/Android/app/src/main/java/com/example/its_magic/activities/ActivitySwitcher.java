ad781a47831eda40139d72adc2be9df2ec80686d
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2024-12-22 10:57:50 +0100

    feat: add fade transitions when scene switched

--- Client/Android/app/src/main/java/com/example/its_magic/activities/ActivitySwitcher.java
+++ Client/Android/app/src/main/java/com/example/its_magic/activities/ActivitySwitcher.java
@@ -1,18 +1,23 @@
 package com.example.its_magic.activities;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+
+import com.example.its_magic.R;
 
 public class ActivitySwitcher {
 
     public static void switchActivity(Context context, Class<?> activityClass) {
         Intent intent = new Intent(context, activityClass);
-
-        if (!(context instanceof android.app.Activity)) {
-            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        }
-
         context.startActivity(intent);
+
+        if (context instanceof Activity) {
+            ((Activity) context).overridePendingTransition(
+                    R.anim.fade_in_anim,
+                    R.anim.fade_out_anim
+            );
+        }
     }
 
     public static void switchActivityWithExtras(Context context, Class<?> targetActivity, Intent extras) {
