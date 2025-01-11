c4d27dddcfaf53fc29628f6468e0e343fe4a3ecb
Orfila Marie <marie.orfila@etu.univ-cotedazur.fr>
2025-01-11 13:59:48 +0100

    feat: add new web socket messages

---
+++ Client/unity/Assets/Scripts/Client/SwitchSceneMessage.cs
@@ -0,0 +1,15 @@
+﻿namespace Client
+{
+    public class SwitchSceneMessage : Message
+    {
+        public string sceneName;
+        
+        public SwitchSceneMessage()
+        {
+            clientId = "TopView";
+            recipientId = "Android";
+            type = "switchObject";
+        }
+        
+    }
+}
