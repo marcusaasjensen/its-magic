fe66b19fc2da44e3418be1903d263182c2a2e03a
Marcus Aas Jensen <aasjensenm@gmail.com>
2025-01-17 23:52:46 +0100

    feat: cauldron showing in side view when snapped

---
+++ Client/unity/Assets/Scripts/Environment/Cauldron.cs
@@ -0,0 +1,41 @@
+﻿using Client;
+using UnityEngine;
+
+namespace Environment
+{
+    public class Cauldron : MonoBehaviour
+    {
+        [SerializeField] private GameObject sideCauldron;
+        public void PositionCauldron()
+        {
+            var cauldronMessage = new CauldronSnapMessage
+            {
+                isSnapped = true
+            };
+            
+            WebSocketClient.Instance.SendMessage(JsonUtility.ToJson(cauldronMessage));
+        }
+        
+        public void RemoveCauldron()
+        {
+            var cauldronMessage = new CauldronSnapMessage
+            {
+                isSnapped = false
+            };
+            
+            WebSocketClient.Instance.SendMessage(JsonUtility.ToJson(cauldronMessage));
+        }
+
+        public void UpdateSideCauldron(string message)
+        {
+            var cauldronSnapMessage = JsonUtility.FromJson<CauldronSnapMessage>(message);
+            if(cauldronSnapMessage is not { type: "CauldronSnap" })
+            {
+                return;
+            }
+            
+            sideCauldron.SetActive(cauldronSnapMessage.isSnapped);
+        }
+        
+    }
+}
