9abe8f4e508681b637d27a32c062b3c76b864980
Marcus Aas Jensen <aasjensenm@gmail.com>
2025-01-18 13:07:45 +0100

    feat: bloe message

---
+++ Client/unity/Assets/Scripts/Managers/FireTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Client;
+using UnityEngine;
+
+namespace Managers
+{
+    public class FireTrigger : MonoBehaviour
+    {
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Bellows"))
+            {
+                EnableBlow(true);
+            }
+        }
+        
+        public void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Bellows"))
+            {
+                EnableBlow(false);
+            }
+        }
+        
+        private static void EnableBlow(bool blow)
+        {
+            var bellowMessage = new BellowsMessage
+            {
+                isBlowingInFire = blow
+            };
+            
+            WebSocketClient.Instance.SendMessageToServer(JsonUtility.ToJson(bellowMessage));
+        }
+    }
+}
